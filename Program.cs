3e6f34382fdabbd6f7dd34a01d7ee0f602c7c7b5
Alonso <alonso.aquila@outlook.it>
2023-10-10 16:12:50 -0500

    Добавлен код решения задачи в файле Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿string[] arrayOne = new string[5] {"Кто", "сказал", "мяу", "?", "!"};
+string[] arrayTwo = new string[arrayOne.Length];
+
+void ArrayOfThreeElements(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+    if(arrayOne[i].Length <= 3)
+        {
+        arrayTwo[count] = arrayOne[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArrayOfThreeElements(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
+
